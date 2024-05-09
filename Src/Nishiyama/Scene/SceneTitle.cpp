189d12ea80e81141941c6b30dd9a9938f6f58c53
ankohina <144640312+ankohina@users.noreply.github.com>
2024-05-09 16:55:11 +0900

    背景移動

--- Src/Nishiyama/Scene/SceneTitle.cpp
+++ Src/Nishiyama/Scene/SceneTitle.cpp
@@ -6,17 +6,29 @@
 
 int PlayNumber = 1;
 
+
 //タイトル初期化
 void SceneTitle::Init()
 {
 	SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_LOOP_TITLE;
 
-	Title_Hndl = LoadGraph(TITLE_PATH);
+
+	for (int i = 0; i < 2; i++)
+	{
+		BackGround_handle[i] = LoadGraph(TITLE_PATH);
+		BackGround_handle2[i] = LoadGraph(TITLE2_PATH);
+	}
+	
 
 	Playbutton_Hndl = LoadGraph(BUTTON_PATH);
 
 	button_x = 440;
 	button_y = 400;
+
+	bgSkyX[0] = 0;
+	bgSkyX[1] = -SCREEN_SIZE_X;
+	bgSkyX1[0] = 0;
+	bgSkyX1[1] = -SCREEN_SIZE_X;
 
 	nextScene = SCENEID::SCENE_ID_INIT_PLAY;
 }
@@ -45,13 +57,33 @@ void SceneTitle::Step()
 		PlayNumber = 1;
 	}
 
-	
+	//背景移動処理
+	for (int i = 0; i < 2; i++)
+	{
+		bgSkyX[i] -= 2;
+		bgSkyX1[i] -= 2;
+
+		if (bgSkyX[i] <= -1280)
+		{
+			bgSkyX[i] = 1270;
+		}
+		if (bgSkyX1[i] <= -1280)
+		{
+			bgSkyX1[i] = 1270;
+		}
+	}
 }
 
 //タイトル描画処理
 void SceneTitle::Draw()
 {
-	DrawGraph(0, 0, Title_Hndl, true);
+	//タイトル画像表示
+	for (int i = 0; i < 2; i++)
+	{
+		DrawGraph(bgSkyX[i], 0, BackGround_handle[i], true);
+		DrawGraph(bgSkyX1[i], 0, BackGround_handle2[i], true);
+	}
+
 	DrawGraph(button_x, button_y, Playbutton_Hndl, true);
 
 	DrawFormatString(0, 0, GetColor(255, 255, 255), "プレイヤーの人数：%d", PlayNumber);
--- Src/Nishiyama/Scene/SceneTitle.h
+++ Src/Nishiyama/Scene/SceneTitle.h
@@ -1,14 +1,21 @@
 #pragma once
 #include "Scene.h"
 
-#define TITLE_PATH		"Data/Title/タイトル.png"
+#define TITLE_PATH		"Data/Title/Title1.png"
+#define TITLE2_PATH		"Data/Title/Title2.png"
 #define BUTTON_PATH		"Data/Title/プレイボタン.png"
 
 //シーン情報
 class SceneTitle : public SceneBase
 {
 private:
-	int Title_Hndl;
+	
+	//背景のグローバル変数
+	int BackGround_handle[2] = { 0 };
+	int bgSkyX[2] = { 0 };
+
+	int BackGround_handle2[2] = { 0 };
+	int bgSkyX1[2] = { 0 };
 
 	int Playbutton_Hndl;
 
